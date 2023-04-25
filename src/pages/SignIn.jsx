81cd9821f797d363956139b631bdd9bba4c875bd
Jonatan Kwiatkowski <joniqs91@gmail.com>
2023-04-25 17:16:13 +0100

    Added google Auth sign in, sign up functionality.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import {Link, useNavigate} from "react-router-dom"
 import {getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import OAuth from "../components/OAuth";
 import {ReactComponent as ArrowRightIcon} from "../assets/svg/keyboardArrowRightIcon.svg"
 import visibilityIcon from "../assets/svg/visibilityIcon.svg"
 import { toast } from "react-toastify";
+
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false)
@@ -85,7 +87,7 @@ const SignIn = () => {
           </button>
         </div>
       </form>
-    { /* Google OAuth */}
+    <OAuth />
     <Link to="/sign-up" className="registerLink">
       Sign Up Instead
     </Link>
