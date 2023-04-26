b27fbc8b1bf092ce1a53ee9b535e8816fbd56d7c
Jonatan Kwiatkowski <joniqs91@gmail.com>
2023-04-26 16:48:40 +0100

    Added listingItem component.

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,19 +1,19 @@
-import { useEffect,useState } from "react"
-import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export const useAuthStatus = () => {
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [checkingStatus, setCheckingStatus] = useState(true)
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
 
   useEffect(() => {
-    const auth = getAuth()
+    const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
-      if(user) {
-        setLoggedIn(true)
+      if (user) {
+        setLoggedIn(true);
       }
-      setCheckingStatus(false)
-    })
-  })
+      setCheckingStatus(false);
+    });
+  });
 
-  return  {loggedIn, checkingStatus}
-}
+  return { loggedIn, checkingStatus };
+};
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,63 +1,91 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import {collection, getDocs, query, where, orderBy, limit, startAfter} from "firebase/firestore"
-import {db} from "../firebase.config"
-import {toast} from "react-toastify"
-import Spinner from "../components/Spinner"
-const Category = () => {
-  const [listings, setListings] = useState(null)
-  const [loading, setLoading] = useState(true)
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import Spinner from '../components/Spinner';
+import ListingItem from '../components/ListingItem';
 
-  const params = useParams()
+const Category = () => {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
 
   useEffect(() => {
     const fetchListings = async () => {
       try {
         //Get reference
-        const listingsRef = collection(db, "listings")
+        const listingsRef = collection(db, 'listings');
 
         //Create a query
-        const q = query(listingsRef, where("type", "==", params.categoryName), orderBy("timestamp", "desc"), limit(10))
+        const q = query(
+          listingsRef,
+          where('type', '==', params.categoryName),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        );
 
         //Execute query
-        const querySnap = await getDocs(q)
+        const querySnap = await getDocs(q);
 
-        const listings = []
+        const listings = [];
 
         querySnap.forEach((doc) => {
           return listings.push({
             id: doc.id,
-            data: doc.data()
-          })
-        })
+            data: doc.data(),
+          });
+        });
 
-        setListings(listings)
-        setLoading(false)
+        setListings(listings);
+        setLoading(false);
       } catch (error) {
-        toast.error("Couldn't get listings, Sorry!")
+        toast.error("Couldn't get listings, Sorry!");
       }
-    }
+    };
 
-    fetchListings()
-  }, [params.categoryName])
-  return <div className="category">
-    <header>
-      <p className="pageHeader">
-        {params.categoryName === "rent" ? "Places for rent" : "Places for sale"}
-      </p>
-    </header>
+    fetchListings();
+  }, [params.categoryName]);
+  return (
+    <div className='category'>
+      <header>
+        <p className='pageHeader'>
+          {params.categoryName === 'rent'
+            ? 'Places for rent'
+            : 'Places for sale'}
+        </p>
+      </header>
 
-    {loading ? <Spinner /> : listings && listings.length > 0 ?
-    <>
-    <main>
-      <ul className="categoryListings">
-        {listings.map((listing) => (
-          <h3 key={listing.id}>{listing.data.name}</h3>
-        ))}
-      </ul>
-    </main>
-    </> : <p>No listings for {params.categoryName}</p>}
-  </div>
-}
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className='categoryListings'>
+              {listings.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No listings for {params.categoryName}</p>
+      )}
+    </div>
+  );
+};
 
-export default Category
+export default Category;
